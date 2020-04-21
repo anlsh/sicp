a7715c62ced1163a2bdf156090d1fadddda5cea0
Anish Moorthy <anlsh@protonmail.com>
2020-04-21 18:02:26 -0400

    Holy magoly generics work

--- generics.lisp
+++ generics.lisp
@@ -8,10 +8,16 @@
 
   (declare (ignore type-hierarchy))
 
-  (defun get-type-table () type-table)
-  (defun get-generic-defs () generic-defs)
+  ;; Internal functions related to typing
+  (defun construct (type val)
+    (unless (gethash type type-table) (error "Attempting to construct undeclared type"))
+    (cons type val))
+  (defun get-type (object)
+    (car object))
+  (defun get-val (object)
+    (cdr object))
 
-  (defun make-type (tname)
+  (defun define-type (tname)
     (multiple-value-bind (_ exists) (gethash tname type-table)
       (declare (ignore _))
       (if exists (error "Type redefinition") (sethash (tname t) type-table))))
@@ -22,10 +28,13 @@
         (volt:transpose argspecs)
       ;; Make sure that all the types actually exist!
       (mapcar (lambda (type)
-                (unless (second (multiple-value-list (gethash type type-table)))
+                (unless (gethash type type-table)
                   (error "Nonexistent type")))
               arg-types)
-      ;; If it's a new generic function, add an entry to the table
       (put-if-absent (name {}) generic-defs)
-      ;; Add an entry for the generic function with the given arguments
-      (sethash (arg-types `(lambda ,arg-names ,@body)) (gethash name generic-defs)))))
+      (sethash arg-types (eval `(lambda ,arg-names ,@body)) (gethash name generic-defs))))
+
+  (defun apply-generic (name &rest args)
+    (let ((types (mapcar #'get-type args))
+          (vals (mapcar #'get-val args)))
+      (apply (gethash types (gethash name generic-defs)) vals))))
