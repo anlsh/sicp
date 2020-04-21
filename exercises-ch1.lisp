18e85b7412eced6aa5be9b249c70c634fed36e56
Anish Moorthy <anlsh@protonmail.com>
2020-04-21 17:16:39 -0400

    Put things in labels

--- exercises-ch1.lisp
+++ exercises-ch1.lisp
@@ -26,32 +26,28 @@
 
 ;; Section 2
 
-(defun improve (guess x)
-  (/ (+ guess (/ x guess)) 2))
-
-(defun square (x)
-  (* x x))
-
-(defun good-enough? (guess x)
-  (< (abs ( - (square guess) x)) 0.001))
-
-(defun sqrt-iter (guess x)
-  (if (good-enough? guess x)
-      guess
-      (sqrt-iter (improve guess x) x)))
-
-(defun my-sqrt (x) (sqrt-iter 1.0 x))
-
 (defun exercise-6 ()
   "Infinite loop: both branches are always taken, and so it continues trying
 to improve guess forever")
 
-(defun exercise-7 ()
-  (format t "~a ~a ~a~%"
-          "Showing that it fails for small numbers is easy! For instance, sqrt(1e-8) = 1-e4 but"
-          (my-sqrt .00000001) "is nowhere close")
-  ;; The following code illustrates the example: if your guess is (sqrt (- big-number .01))
-  ;; and you're trying to find (sqrt big-number), you'll have issues
-  (let ((big-number (expt 10 10))) (- big-number (square (sqrt (- big-number .01)))))
-  ;; Not gonna do this last part
-  )
+(labels ((improve (guess x)
+           (/ (+ guess (/ x guess)) 2))
+         (square (x)
+           (* x x))
+         (good-enough? (guess x)
+           (< (abs ( - (square guess) x)) 0.001))
+         (sqrt-iter (guess x)
+           (if (good-enough? guess x)
+               guess
+               (sqrt-iter (improve guess x) x)))
+         (>sqrt (x) (sqrt-iter 1.0 x)))
+
+  (defun exercise-7 ()
+    (format t "~a ~a ~a~%"
+            "Showing that it fails for small numbers is easy! For instance, sqrt(1e-8) = 1-e4 but"
+            (my-sqrt .00000001) "isn't really close")
+    ;; The following code illustrates the example: if your guess is (sqrt (- big-number .01))
+    ;; and you're trying to find (sqrt big-number), you'll have issues
+    (let ((big-number (expt 10 10))) (- big-number (square (sqrt (- big-number .01)))))
+    ;; Not gonna do this last part
+    ))
