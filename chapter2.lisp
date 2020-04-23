5901aaf80c9fe9f2978293377885ad560d109b70
Anish Moorthy <anlsh@protonmail.com>
2020-04-23 00:36:00 -0400

    Allow lexical binds in generic definitions

--- chapter2.lisp
+++ chapter2.lisp
@@ -8,18 +8,15 @@
   (let* ((gcd (gcd (abs n) (abs d)))
          (num (abs (/ n gcd)))
          (dnom (abs (/ d gcd))))
-    (construct :rat
+    (construct :int-ends
                (cond ((> (* n d) 0) (cons num dnom))
                      (t (cons (* -1 num) dnom))))))
 
 ;; Interval Arithmetic
 
-(defun make-int-ends (l r)
-  (construct :int-ends (cons l r)))
-
 (define-generic lower-bound ((:int-ends x))
   (car x))
 
-(define-generic upper-bound ((:int-ends x))
-  (cdr x))
-
+(let ((a 3))
+  (define-generic add-a-lower ((:int-ends x))
+    (+ a (car x))))
--- generics.lisp
+++ generics.lisp
@@ -5,26 +5,29 @@
 (let ((type-table {})
       (generic-defs {}))
 
-  ;; Internal functions related to typing
+  (defun define-type (tname)
+    (sethash (tname t) type-table))
+
   (defun construct (type val)
     (unless (gethash type type-table) (error (format nil "Nonexistent type ~a" type)))
     (cons type val))
-
-  (defun define-type (tname)
-    (sethash (tname t) type-table))
 
   (labels ((get-type (object) (car object))
            (get-val (object) (cdr object))
            (apply-generic (name args)
              (let ((types (mapcar #'get-type args))
                    (vals (mapcar #'get-val args)))
-               (multiple-value-bind (fn exists) (gethash types (gethash name generic-defs))
-                 (unless exists
-                   (error (format nil "~a has no definition for ~a~%" name types)))
-                 (apply fn vals)))))
+               (multiple-value-bind (fn-table exists) (gethash name generic-defs)
+                 (unless exists (error (format nil "~a has no specializations" name)))
+                 (multiple-value-bind (fn exists) (gethash types fn-table)
+                   (unless exists
+                     (error (format nil "~a has no definition for ~a" name types)))
+                   (apply fn vals))))))
 
     (defmacro define-generic (name argspecs &body body)
+
       (put-if-absent (name {}) generic-defs)
+
       (destructuring-bind
           (arg-types arg-names)
           (transpose argspecs)
@@ -33,6 +36,8 @@
                   (unless (gethash type type-table)
                     (error (format nil "Nonexistent type ~a" type))))
                 arg-types)
-        (sethash (arg-types (eval `(lambda ,arg-names ,@body))) (gethash name generic-defs))
-        ;; Enable calling generics as you would other functions
-        (setf (symbol-function name) (lambda (&rest args) (apply-generic name args)))))))
+
+        ;; Allows generic functions to access lexical scope
+        `(progn (setf (symbol-function ',name) ,(lambda (&rest args) (apply-generic name args)))
+                (setf (gethash ',arg-types (gethash ',name ,generic-defs))
+                      (lambda ,arg-names ,@body)))))))
